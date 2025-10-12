9b84cfd6cccaee3b8ae9fcd8f444e2ae1ce01903
Rudra Dhamecha <rudra@unifyxcess.ai>
2025-10-12 19:54:14 +0530

    feat: add API to get Questions from category

--- src/main/java/com/devit/quizapp/repository/QuestionRepository.java
+++ src/main/java/com/devit/quizapp/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package com.devit.quizapp.repository;
 import com.devit.quizapp.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    List<Question> findByCategory(String category);
 }
